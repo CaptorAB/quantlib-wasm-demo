27c7527db3848a833d1dea974585ac3ec2c02c89
Andreas Lindh <andreas.lindh@loopstone.com>
2024-08-12 10:44:47 +0200

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     return {
+        base: "/quantlib-wasm-demo/", // your repository name
         plugins: [react()],
         build: {
             sourcemap: true,
